6642d62ee278269c3c16e8f561b4a6baf4d63011
Ronan F. <ronan.fesselier@etik.com>
2026-02-19 11:22:06 +0100

    feat: force discrete GPU on Windows and Linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include "Application.h"
 
+#ifdef _WIN32
+extern "C"
+{
+    __declspec( dllexport ) unsigned long NvOptimusEnablement = 1;
+    __declspec( dllexport ) int AmdPowerXpressRequestHighPerformance = 1;
+}
+#endif
+
+#ifdef __linux__
+#include <cstdlib>
+#endif
+
 int main()
 {
+#ifdef __linux__
+    setenv( "__NV_PRIME_RENDER_OFFLOAD", "1", 1 );
+    setenv( "__GLX_VENDOR_LIBRARY_NAME", "nvidia", 1 );
+    setenv( "DRI_PRIME", "1", 1 );
+#endif
+
     nfx::vista::Application app;
     app.initialize();
 
